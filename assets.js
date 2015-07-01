7dbff2b972c043219e81d3db93f658638ea9680b
Matteo Collina <hello@matteocollina.com>
2015-07-01 20:20:51 +0200

    Hello HAPI!

--- assets.js
+++ assets.js
@@ -1,6 +1,46 @@
 'use strict'
 
-function assets () {
+var Hapi = require('hapi')
+var xtend = require('xtend')
+var minimist = require('minimist')
+var defaults = {
+  port: 8989
 }
 
-module.exports = assets
+function assetsService (opts) {
+  opts = xtend(defaults, opts)
+
+  var server = new Hapi.Server()
+
+  server.connection({ port: opts.port })
+
+  function hello (request, reply) {
+    reply('Hello World')
+  }
+
+  server.route({ method: 'GET', path: '/', handler: hello })
+
+  return server
+}
+
+function start (opts) {
+  var server = assetsService(opts)
+  server.start(function (err) {
+    if (err) { throw err }
+
+    server.connections.forEach(function (conn) {
+      console.log('server started on port', conn.settings.port)
+    })
+  })
+}
+
+module.exports = assetsService
+
+if (require.main === module) {
+  start(minimist(process.argv.slice(2), {
+    integer: 'port',
+    alias: {
+      'port': 'p'
+    }
+  }))
+}
--- test.js
+++ test.js
@@ -1,9 +1,18 @@
 'use strict'
 
-var test = require('tape')
-var assets = require('./')
+var code = require('code')
+var Lab = require('lab')
+var lab = exports.lab = Lab.script({ output: process.stdout })
+var assetsService = require('./')
 
-test('assets is a function', function (t) {
-  t.equal(typeof assets, 'function', 'is a function')
-  t.end()
+lab.experiment('Hello', function () {
+  lab.test('Testing for "Hello World"', function (done) {
+    var server = assetsService({ port: 8989 })
+    var options = { method: 'GET', url: '/' }
+    server.inject(options, function (response) {
+      var result = response.result
+      code.expect(result).to.equal('Hello World')
+      done()
+    })
+  })
 })
